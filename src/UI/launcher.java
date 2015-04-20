aa76249dc38f9e8152db7ac6a69e2dc5d804d162
schnip <pingthegreenflag@gmail.com>
2015-04-20 19:36:36 -0400

    working on stuff

--- src/UI/launcher.java
+++ src/UI/launcher.java
@@ -8,6 +8,9 @@ public class launcher {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		for (int i = 0;i<40;i++) {
+			System.out.print("XH");
+		}
 	}
 
 }
