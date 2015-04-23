337ce25b7030d2c02b4756b8f3ca452c737246e8
maurerja <maurerja@rose-hulman.edu>
2015-04-22 23:18:23 -0400

    updated database connection with table connect

--- src/database/JavaToMySQL.java
+++ src/database/JavaToMySQL.java
@@ -4,30 +4,56 @@ import java.sql.*;
 
 public class JavaToMySQL {
 	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
-	   static final String DB_URL = "jdbc:mysql://localhost/";
+	static final String DB_URL = "jdbc:mysql://localhost/";
 
-	   //  Database credentials
-	   static final String USER = "root";
-	   static final String PASS = "pass";
+	//  Database credentials
+	static final String USER = "root";
+	static final String PASS = "pass";
+	static Connection conn = null;
+	static Statement stmt = null;
 	public static void main(String[] args) throws SQLException, InstantiationException, IllegalAccessException, ClassNotFoundException{
-		Connection conn = null;
-		   Statement stmt = null;
-		   Class.forName("com.mysql.jdbc.Driver");
+		Class.forName("com.mysql.jdbc.Driver");
 
-		      //STEP 3: Open a connection
-		      System.out.println("Connecting to database...");
-		      conn = DriverManager.getConnection(DB_URL, USER, PASS);
+		//STEP 3: Open a connection
+		System.out.println("Connecting to database...");
+		conn = DriverManager.getConnection(DB_URL, USER, PASS);
 
-		      //STEP 4: Execute a query
-		      System.out.println("Creating database...");
-		      stmt = conn.createStatement();
-		      
-		      String sql = "DROP DATABASE Noire_Etoile";
-		      stmt.executeUpdate(sql);
-		      
-		      sql = "CREATE DATABASE Noire_Etoile";
-		      stmt.executeUpdate(sql);
-		      System.out.println("Database created successfully...");
-	
+		//STEP 4: Execute a query
+		System.out.println("Creating database...");
+		stmt = conn.createStatement();
+		boolean hasDatabase = false;
+		ResultSet resultSet = conn.getMetaData().getCatalogs();
+		String sql;
+		while (resultSet.next()) {
+
+			String databaseName = resultSet.getString(1);
+			if(databaseName.equals("noire_etoile")){
+				hasDatabase=true;
+			}
+		}
+		resultSet.close();
+		if (!hasDatabase){
+			sql = "CREATE DATABASE Noire_Etoile";
+			stmt.executeUpdate(sql);
+			System.out.println("Database created successfully...");
+
+
+		}else System.out.println("Database already exists...");
+		createTable("test",
+				"(id INTEGER not NULL, PRIMARY KEY ( id ))");
+		DatabaseMetaData meta = conn.getMetaData();
+		ResultSet res = meta.getTables("noire_etoile", null, "test",null);
+		if (res.next())System.out.println("table created");
+		else System.out.println("no table created");
+		
+		
+
+	}
+
+	public static int createTable(String tableName,String properties) throws SQLException{
+		String sql;
+		stmt.executeUpdate("use noire_etoile");
+		sql = "CREATE TABLE IF NOT EXISTS "+ tableName+ " " + properties;
+		return stmt.executeUpdate(sql);
 	}
 }
