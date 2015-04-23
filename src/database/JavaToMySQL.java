980eb4f50f43e3fed7956177246c9c343943926c
maurerja <maurerja@rose-hulman.edu>
2015-04-22 20:11:11 -0400

    set up the initial database

--- src/database/JavaToMySQL.java
+++ src/database/JavaToMySQL.java
@@ -1,5 +1,33 @@
 package database;
 
-public class JavaToMySQL {
+import java.sql.*;
 
+public class JavaToMySQL {
+	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
+	   static final String DB_URL = "jdbc:mysql://localhost/";
+
+	   //  Database credentials
+	   static final String USER = "root";
+	   static final String PASS = "pass";
+	public static void main(String[] args) throws SQLException, InstantiationException, IllegalAccessException, ClassNotFoundException{
+		Connection conn = null;
+		   Statement stmt = null;
+		   Class.forName("com.mysql.jdbc.Driver");
+
+		      //STEP 3: Open a connection
+		      System.out.println("Connecting to database...");
+		      conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+		      //STEP 4: Execute a query
+		      System.out.println("Creating database...");
+		      stmt = conn.createStatement();
+		      
+		      String sql = "DROP DATABASE Noire_Etoile";
+		      stmt.executeUpdate(sql);
+		      
+		      sql = "CREATE DATABASE Noire_Etoile";
+		      stmt.executeUpdate(sql);
+		      System.out.println("Database created successfully...");
+	
+	}
 }
