69b86ef0eae9ddad979dda13fb5f9ea4fc7ca457
downinew <downinew@rose-hulman.edu>
2015-04-28 23:55:35 -0400

    gamedata create scripts

---
+++ SQL_Scripts/GameData.sql
@@ -0,0 +1,24 @@
+/* Inserting data into the data base */
+
+
+INSERT INTO Planet
+(name, loc_x, loc_y, info, danger_level, police_level)
+VALUES
+('omicron persei 8', 0, 0, "Lrrr doesn't like unwanted guests", 10, 10);
+
+INSERT INTO Planet
+(name, loc_x, loc_y, info, danger_level, police_level)
+VALUES
+('Tatooine', 10, 10, "Maybe vader some day later now he's just a small fry", 2, 2);
+
+INSERT INTO Planet
+(name, loc_x, loc_y, info, danger_level, police_level)
+VALUES
+('Planet Eric', 20, 20, "You fools! HAHAHA", 100, 100);
+
+INSERT INTO Star_System
+(name, loc_x, loc_y, info, danger_level, police_level)
+VALUES
+('Easter Egg Nebula', 0,0, "Check out this!", 66, 66);
+
+
