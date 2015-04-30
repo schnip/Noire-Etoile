c91366c92205094d2cd415a6f8b8b1be82a491a2
schnip <pingthegreenflag@gmail.com>
2015-04-29 23:03:00 -0400

    added more entries to game data

--- SQL_Scripts/GameData.sql
+++ SQL_Scripts/GameData.sql
@@ -26,4 +26,27 @@ INSERT INTO Star_log
 VALUES
 ('Eric was here but this was written by Josh',200,'Planet Eric',null);
 
+INSERT INTO Star_log
+(log_text, star_date, planet, star_system)
+VALUES
+('Eric was here but this was written by Schnipke',201,'Planet Eric',null);
 
+INSERT INTO Star_log
+(log_text, star_date, planet, star_system)
+VALUES
+('Eric was here but this was written by Pie',210,'Planet Eric',null);
+
+INSERT INTO Star_log
+(log_text, star_date, planet, star_system)
+VALUES
+('Eric was here but this was written by Banana',195,'Planet Eric',null);
+
+INSERT INTO Star_log
+(log_text, star_date, planet, star_system)
+VALUES
+('Eric was here but this was written by Grapes',215,'Planet Eric',null);
+
+INSERT INTO Star_log
+(log_text, star_date, planet, star_system)
+VALUES
+('Eric was here but this was written by Cherry',202,'Planet Eric',null);
