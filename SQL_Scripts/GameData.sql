46113826a56b270ce07da615cd26f5c1c3fd49e6
maurerja <maurerja@rose-hulman.edu>
2015-05-07 14:17:36 -0400

    added data

--- SQL_Scripts/GameData.sql
+++ SQL_Scripts/GameData.sql
@@ -6,15 +6,25 @@ INSERT INTO Star_System
 VALUES
 ('Easter Egg Nebula', 0,0, "Check out this!", 66, 66);
 
+INSERT INTO Star_System
+(name, loc_x, loc_y, info, danger_level, police_level)
+VALUES
+('Larry is going crazy', 30,50, "He is freaking out!", 40, 20);
+
 INSERT INTO Planet
 (name, loc_x, loc_y, info, danger_level, police_level,star_system)
 VALUES
 ('Omicron Persei 8', 0, 0, "Lrrr doesn't like unwanted guests", 10, 10,'Easter Egg Nebula');
 
 INSERT INTO Planet
-(name, loc_x, loc_y, info, danger_level, police_level)
+(name, loc_x, loc_y, info, danger_level, police_level,star_system)
 VALUES
-('Tatooine', 10, 10, "Maybe vader some day later now he's just a small fry", 2, 2);
+('Tatooine', 10, 10, "Maybe vader some day later now he's just a small fry", 2, 2,'Larry is going crazy');
+
+INSERT INTO Planet
+(name, loc_x, loc_y, info, danger_level, police_level,star_system)
+VALUES
+('Sriram Land', 10, 10, "The best place in the galaxy", 2, 2,'Larry is going crazy');
 
 INSERT INTO Planet
 (name, loc_x, loc_y, info, danger_level, police_level,star_system)
