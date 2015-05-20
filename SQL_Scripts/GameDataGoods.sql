65b8741edd1f7e6cea060fddd79e30ef239e8170
downinew <downinew@rose-hulman.edu>
2015-05-20 19:38:35 -0400

    adding in some new goods!

--- SQL_Scripts/GameDataGoods.sql
+++ SQL_Scripts/GameDataGoods.sql
@@ -32,3 +32,143 @@ insert into Goods
 (goodName,good_value,weight,legality,description)
 values
 ('Rifle Blaster', 250, 2,true,'This is my rifle, THAT is my gun...');
+
+insert into Goods
+(goodName,good_value,weight,legality,description)
+values
+('Old NASA satellite', 50, 200,true, 'Old outdated technology from a forgotten administration');
+
+insert into Goods
+(goodName,good_value,weight,legality,description)
+values
+('Queen Elizabeth''s Crown Jewels', 100000, 1, false, 'HOW DID YOU GET THESE?!');
+
+insert into Goods
+(goodName,good_value,weight,legality,description)
+values
+('Ammo', 10, 1, true, 'That gun is probably gonna need some ammo...');
+
+insert into Goods
+(goodName,good_value,weight,legality,description)
+values
+('Scheme Interpreter', 0, 1, true, 'No body needs this in their life...');
+
+insert into Goods
+(goodName,good_value,weight,legality,description)
+values
+('Raptor Eggs', 1000, 2, true, 'I''d hate to run into mama raptor');
+
+insert into Goods
+(goodName,good_value,weight,legality,description)
+values
+('Celestial Sword', 100, 7,true, 'New and improved this sword will slice any material in the universe! Except for Nokia');
+
+insert into Goods
+(goodName,good_value,weight,legality,description)
+values
+('Nokia Cell Phone Armor', 50, 50, true, 'How much would you put on invulnerability? We say 50 bucks');
+
+insert into Goods
+(goodName,good_value,weight,legality,description)
+values
+('Alien Brain', 25, 2,true, 'Quite useful for dissection purposes');
+
+insert into Goods
+(goodName,good_value,weight,legality,description)
+values
+('Human Horn Aphrodisiac', 500, 1, false, 'This is and should be illegal');
+
+insert into Goods
+(goodName,good_value,weight,legality,description)
+values
+('Monstrous Eye', 100, 3, true, 'This eye is quite monstrous, wouldnt you say?');
+
+insert into Goods
+(goodName,good_value,weight,legality,description)
+values
+('Woodchuck wood', 10, 5, true, 'How much wood could a woodchuck chuck...');
+
+insert into Goods
+(goodName,good_value,weight,legality,description)
+values
+('Answer to the Universe', 42, 42, false, 'NO ONE IS SUPPOSE TO KNOW! *cough* 42 *cough*');
+
+insert into Goods
+(goodName,good_value,weight,legality,description)
+values
+('Phaser', 400, 3, true, 'Set to stun or kill... your choice');
+
+insert into Goods
+(goodName,good_value,weight,legality,description)
+values
+('Oregon Trail', 5, 1, true, 'It would be cool if they made a space version of this game');
+
+insert into Goods
+(goodName,good_value,weight,legality,description)
+values
+('Old Planetary Scanner', 75, 5, true, 'Old but still useful');
+
+insert into Goods
+(goodName,good_value,weight,legality,description)
+values
+('Dragon Bits', 600, 20, false, 'Why would you need the bits of a dragon? This isn''t Dragon-Age');
+
+insert into Goods
+(goodName,good_value,weight,legality,description)
+values
+('Metorite Fragment', 800, 2, true, 'Might be useful, or something');
+
+insert into Goods
+(goodName,good_value,weight,legality,description)
+values
+('Pokedex', 300, 1, true, 'Try to catch them all');
+
+insert into Goods
+(goodName,good_value,weight,legality,description)
+values
+('Someone elses passport', 100, 1, false, 'This is highly illegal...');
+
+insert into Goods
+(goodName,good_value,weight,legality,description)
+values
+('Alcoholic Beverage', 20, 3, false, 'ARE YOU OF AGE? Our scanners say no');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
